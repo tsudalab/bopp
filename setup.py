b74094898f3a2fe0839b5f6697f7956e2a61d632
ThaerDieb <thaer.dieb@gmail.com>
2019-07-04 16:24:34 +0900

    Setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='bopp',
+      version='0.1',
+      description='Black-box optimization of peptides and proteins',
+      url='https://github.com/tsudalab/bopp',
+      author='Tran Phuoc Duy',
+      author_email='tpduy@bio.titech.ac.jp',
+      license='MIT',
+      packages=['bopp'],
+      zip_safe=False)
