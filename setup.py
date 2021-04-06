a3e9259e05fea413854b09f9e8cd7215587bd430
duy <tranpduy87@gmail.com>
2021-04-06 10:50:10 +0900

    update setup.py for AMBERTools

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 import pkg_resources
 
-_REQUIREMENTS_PATH = Path(__file__).parent.with_name("requirements.txt")
+#_REQUIREMENTS_PATH = Path(__file__).parent.with_name("requirements.txt")
 
 
 class TestRequirements(unittest.TestCase):
     def test_requirements(self):
-        requirements = pkg_resources.parse_requirements(_REQUIREMENTS_PATH.open())
+        requirements = pkg_resources.parse_requirements("requirements.txt")
         for requirement in requirements:
             requirement = str(requirement)
             with self.subTest(requirement=requirement):
@@ -25,7 +25,7 @@ for x in "gmx_mpi" "gmx" "gmx_mpi_d" "gmx_gpu":
             GROtest=True
             GROpath=ln.strip().split()[1]
             print("Modify the GROMACS command in parameter.py file with "+GROpath)
-            break()
+            break
         else:
             print(x+" : failed ...")
 
@@ -44,33 +44,37 @@ if GROtest==False:
     os.chdir(pwd)
 
 #checking whether AMBER is working or not.
-GROtest=False
-print("Testing whether tleap in AMBER package is installed")
-for x in "tleap" "gmx" "gmx_mpi_d" "gmx_gpu":
+print("Testing whether tleap, amb2gro_top_gro.py in AMBERTools package is installed")
+tleaptest=False
+for x in "tleap":
     os.system(x+" -h > test")
     with open('test') as f:
         ln=f.read()
-        if 'Executable' in ln:
-            GROtest=True
-            GROpath=ln.strip().split()[1]
-            print("Modify the GROMACS command in parameter.py file with "+GROpath)
-            break()
+        if 'Usage' in ln:
+            tleaptest=True
+            tleappath=ln.strip().split()[1]
         else:
             print(x+" : failed ...")
+#clean trash
+os.system("rm -r test")
+
+amb2grotest=False
+for x in "tleap":
+    os.system(x+" -h > test")
+    with open('test') as f:
+        ln=f.read()
+        if 'Usage' in ln:
+            amb2grotest=True
+            amb2gropath=ln.strip().split()[1]
+        else:
+            print(x+" : failed ...")
+#clean trash
+os.system("rm -r test")
 
 pwd=os.getcwd()
-GROtest=False
-if GROtest==False:
-    os.system("wget http://ftp.gromacs.org/pub/gromacs/gromacs-2021.1.tar.gz ")
-    os.system("tar -xvzf gromacs-2021.1.tar.gz ")
-    os.system("mkdir gromacs-2021.1/build")
-    os.system("mkdir soft")
-    os.system("mkdir soft/gromacs")
-    #os.chdir("gromacs-2019.6/build")
-    os.system("cmake -S "+pwd+"/gromacs-2021.1  -B "+pwd+"/gromacs-2021.1/build -DCMAKE_INSTALL_PREFIX="+pwd+"/soft/gromacs -DGMX_FFT_LIBRARY=fftw3 -DGMX_BUILD_OWN_FFTW=ON -DGMX_MPI=ON -DGMX_GPU=CUDA; ")
-    os.chdir(pwd+"/gromacs-2021.1/build")
-    os.system("make -j4; make install")
-    os.chdir(pwd)
+ambertooltest=amb2grotest or tleap
+if ambertooltest==False:
+    os.system("conda install -c conda-forge ambertools=20 ")
 #clean trash
 os.system("rm -r test")
 
